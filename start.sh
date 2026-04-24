cda92a428f22cbe112b4866bfb5e1127e90027ef
azelmad2004 <mohmadazelmad3@gmail.com>
2026-04-24 17:51:20 +0100

    Fix static files and styling for production

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 echo "Running migrations..."
 python manage.py migrate
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
 echo "Seeding database..."
 python seed_users.py
 python seed_services.py
